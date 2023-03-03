278aa83f45dcf135afcd68c79731ca2f74bddec5
Dragos Danilov <dragosdanilov266@gmail.com>
2023-03-03 15:16:48 -0500

    Enable loop for movie search result output with conditional rendering

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import MovieCard from './MovieCard';
 
@@ -19,11 +19,13 @@ const movie1 = {
 
 const App = () => {
 
+    const [movies, setMovies] = useState()
+
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -47,9 +49,21 @@ const App = () => {
                 />
             </div>
 
-            <div className="container">
-                <MovieCard movie1={movie1}/>
-            </div>
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                            {movies.map((movie) => (
+                                <MovieCard movie={movie} />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className='empty'>
+                            <h2>No movies found</h2>
+                        </div>
+                    )
+
+            }
         </div>
     );
 }
