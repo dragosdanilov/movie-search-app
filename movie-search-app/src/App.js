6b63dcddb12b3b2fa8b2c4317d160d59dde65451
Dragos Danilov <dragosdanilov266@gmail.com>
2023-03-03 03:45:14 -0500

    Add header with app name and search input field and icon

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -7,6 +7,14 @@ import React from 'react';
 
 const API_URL = 'http://www.omdbapi.com?apikey=984309db';
 
+const movie1 = {
+    "Title": "The Batman",
+    "Year": "2022",
+    "imdbID": "tt1877830",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg"
+}
+
 const App = () => {
 
     const searchMovies = async (title) => {
@@ -21,7 +29,22 @@ const App = () => {
     }, [])
 
     return (
-        <h1>App</h1>
+        <div className='app'>
+            <h1>The fliX-Files</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value="Batman"
+                    onChange={() => {}}
+                />
+                <img 
+                src={SearchIcon}
+                alt="search"
+                onClick ={() => {}}
+                />
+            </div>
+        </div>
     );
 }
 
