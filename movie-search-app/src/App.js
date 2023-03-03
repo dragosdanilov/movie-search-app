451116e91c6e4666d84c2195b3ea126bc10a63de
Dragos Danilov <dragosdanilov266@gmail.com>
2023-03-03 15:46:15 -0500

    Add movie search functionality using state hooks

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -19,7 +19,8 @@ const movie1 = {
 
 const App = () => {
 
-    const [movies, setMovies] = useState()
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -39,31 +40,28 @@ const App = () => {
             <div className="search">
                 <input
                     placeholder="Search for movies"
-                    value="Batman"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img 
                 src={SearchIcon}
                 alt="search"
-                onClick ={() => {}}
+                onClick ={() => searchMovies(searchTerm)}
                 />
             </div>
 
-            {
-                movies?.length > 0
-                    ? (
-                        <div className="container">
-                            {movies.map((movie) => (
-                                <MovieCard movie={movie} />
-                            ))}
-                        </div>
-                    ) : (
-                        <div className='empty'>
-                            <h2>No movies found</h2>
-                        </div>
-                    )
-
-            }
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )}
         </div>
     );
 }
