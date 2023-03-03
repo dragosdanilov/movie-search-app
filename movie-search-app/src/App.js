c53082c988797f11dfbdf729039e38f3e50ea4c1
Dragos Danilov <dragosdanilov266@gmail.com>
2023-03-03 02:51:52 -0500

    Integrate and test OMDb API for movie data access

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -1,6 +1,22 @@
+import { useEffect } from 'react';
+
 import React from 'react';
 
+const API_URL = 'http://www.omdbapi.com?apikey=984309db';
+
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Batman');
+    }, [])
+
     return (
         <h1>App</h1>
     );
